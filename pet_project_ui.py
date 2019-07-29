ccdd9252213f5dcdadbae75c6d132fb8bcc1c5e2
Noemi Baranyi <b.noemi89@gmail.com>
2019-07-29 12:37:37 +0200

    print out main menu

---
+++ pet_project_ui.py
@@ -0,0 +1,11 @@
+def print_error_message(message):
+    print(f'Error: @{message}')
+
+
+def print_menu(title, list_options, exit_message):
+    count = 1
+    print("\t" + title)
+    for element in list_options:
+        print(f"\t({count}) {element}")
+        count += 1
+    print(f"\t({0})  {exit_message}")
