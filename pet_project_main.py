93595d0b1ba4cf5bb4b3dc971c44ba629e59f0a0
Bozi Daniel <daniel.bozi88@gmail.com>
2019-07-29 11:48:50 +0200

    menu and sheeeeet

--- pet_project_main.py
+++ pet_project_main.py
@@ -1,7 +1,32 @@
+import pet_project_data_manager
+import pet_project_ui
+import sys
 
 
+def choose():
+    inputs = pet_project_ui.user_input
+    if inputs == "1":
+        pass
+    elif inputs == "2":
+        pass
+    elif inputs == "3":
+        pass
+    elif inputs == "4":
+        pass
+    elif inputs == "5":
+        pass
+    elif inputs == "0":
+        sys.exit()
+    """else:
+        print("wrong input")
+        sys.exit()
+"""
 
 
+def main():
+    print_main()
+    choose()
+    
 
 if __name__ == "__main__":
     main()
