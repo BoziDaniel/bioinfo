ace8d383fb727d412c7ac51eb05d2f5faa7c410c
Bozi Daniel <daniel.bozi88@gmail.com>
2019-07-30 14:01:43 +0200

    corrections

--- pet_project_data_manager.py
+++ pet_project_data_manager.py
@@ -1,5 +1,6 @@
 import pet_project_ui
 import pet_project_AA_code_dictionary
+import re
 
 
 def DNA_complementer_creation(inputs):
@@ -20,8 +21,9 @@ def DNA_complementer_creation(inputs):
 
 
 def DNA_to_RNA(inputs):
+    DNA_complementer = DNA_complementer_creation(inputs)
     RNA_complementer = ""
-    for base in inputs:
+    for base in DNA_complementer:
         if base == "A":
             RNA_complementer += "U"
         elif base == "T":
@@ -41,9 +43,23 @@ def RNA_to_protein(inputs):
     RNA_pcs = [inputs[i:i+n] for i in range(0, len(inputs), n)]
     protein = []
     for triplet in RNA_pcs:
-        protein.append(pet_project_AA_code_dictionary.AA_dict.get(triplet))
-
-    protein = "".join(protein)
-    
+        if len(triplet) < 3:
+            pass
+        else:
+            protein.append(pet_project_AA_code_dictionary.AA_dict.get(triplet))
+    protein = "-".join(protein)
     return protein
 
+
+def DNA_to_protein(inputs):
+    RNA_complementer = DNA_to_RNA(inputs)
+    protein = RNA_to_protein(RNA_complementer)
+    print(count_STOP_in_protein(protein))
+    print(protein)
+    return protein
+
+
+def count_STOP_in_protein(protein):
+    STOPs_in_protein = []
+    STOPs_in_protein.append(re.search("STOP.", protein))
+    return len(STOPs_in_protein)
