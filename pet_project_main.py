9f55b81424aa8b02716cc66eb155fa5bad83fd5e
Noemi Baranyi <b.noemi89@gmail.com>
2019-08-01 13:44:49 +0200

    add clear function

--- pet_project_main.py
+++ pet_project_main.py
@@ -16,27 +16,32 @@ def choose():
         print(f"protein: {protein}\n")
         print(f"STOP in protein: {dm.count_STOP_in_protein(protein)}")
         dm.option_to_save_file(protein)
+        clear()
     elif inputs == "2":
         RNA_sequence = ui.user_input("\nAdd RNA: ", ["A", "U", "C", "G"])
         protein = dm.RNA_to_protein(RNA_sequence)
         print(f"protein: {protein}\n")
         print(f"STOP in protein: {dm.count_STOP_in_protein(protein)}")
         dm.option_to_save_file(protein)
+        clear()
     elif inputs == "3":
         DNA_sequence = ui.user_input("\nAdd DNA: ", ["A", "T", "C", "G"])
         RNA_complementer = dm.DNA_to_RNA(DNA_sequence)
         print(f"RNA: {RNA_complementer}\n")
         dm.option_to_save_file(RNA_complementer)
+        clear()
     elif inputs == "4":
         RNA_sequence = ui.user_input("\nAdd RNA: ", ["A", "U", "C", "G"])
         DNA = dm.RNA_to_DNA(RNA_sequence)
         print(f"DNA: {DNA}\n")
         dm.option_to_save_file(DNA)
+        clear()
     elif inputs == "5":
         DNA_sequence = ui.user_input("\nAdd DNA: ", ["A", "T", "C", "G"])
         complementer = dm.DNA_complementer_creation(DNA_sequence)
         print(f"DNA complementer: {complementer}\n")
         dm.option_to_save_file(complementer)
+        clear()
     elif inputs == "6":
         file_name = input("\nAdd existing file name: ")
         dm.open_existing_file(file_name)
@@ -58,6 +63,7 @@ def handle_menu():
 
 
 def main():
+    clear()
     while True:
         handle_menu()
         try:
